c5a4c95f380136395bfbda9be05dc833b43a2346
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-22 23:45:22 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/\b[A-Z]\w+\W\b/) ? true : false
 end
 
 def valid_phone_number?(phone)
